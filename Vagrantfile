83511d76c7f140bdb36c0af5e222477515e36af4
YarikRevich <yariksvitlitskiy2@gmail.com>
2022-04-22 17:31:57 +0200

    feature: made vagrantfile more lightweight

--- Vagrantfile
+++ Vagrantfile
@@ -8,16 +8,7 @@ Vagrant.configure("2") do |config|
   config.vm.network "forwarded_port", guest_ip: "0.0.0.0", guest: ENV['SERVER_PORT'], host_ip: "0.0.0.0", host: ENV['SERVER_PORT'], protocol: "tcp"
 
   config.vm.provision "docker"
-  config.vm.provision "file", source: "./", destination: "./codenjoy"
-  config.vm.provision "shell", inline: '
-    apt install default-jre -y
-    apt install default-jdk -y
-    apt install maven -y
 
-    cd ./codenjoy
-
-    bash 1-git-clone.sh
-    bash 2-build.sh
-    bash 3-start.sh
-  ', args: ENV['GAMES'], privileged: true
+  config.vm.provision "file", source: ".env", destination: ".env"
+  config.vm.provision "shell", path: "start.sh", privileged: true
 end
--- start.sh
+++ start.sh
@@ -29,39 +29,17 @@ HOME_DIR=$(
     pwd
 )
 
-eval_echo() {
-    to_run=$1
-    echo "[94m"
-    echo $to_run
-    echo "[0m"
-    
-    eval $to_run
-}
-
 if [ ! -d "codenjoy" ]; then
-    GIT_REPO=https://github.com/codenjoyme/codenjoy.git
-    GIT_REVISION=master
-    
-    eval_echo "git clone --recursive $GIT_REPO"
-    eval_echo "git checkout $GIT_REVISION"
+    git clone --recursive https://github.com/codenjoyme/codenjoy.git
+    git checkout master
 fi
 
-if [[ "$(docker images -q $DOCKER_IMAGE 2> /dev/null)" == "" ]]; then
-    DOCKER_IMAGE=apofig/codenjoy-contest:1.1.3
-    
-    eval_echo "docker build --build-arg GAMES=$GAMES -f ./Dockerfile -t $DOCKER_IMAGE ."
-fi
-
-eval_echo "mkdir $HOME_DIR/logs"
-eval_echo "chown $JETTY_PID:$JETTY_PID $HOME_DIR/logs"
-
-eval_echo "touch $HOME_DIR/logs/codenjoy-contest.log"
-eval_echo "chown $JETTY_PID:$JETTY_PID $HOME_DIR/logs/codenjoy-contest.log"
-
-eval_echo "mkdir $HOME_DIR/database"
-eval_echo "chown $JETTY_PID:$JETTY_PID $HOME_DIR/database"
-
-eval_echo "docker rm --force $CONTAINER_NAME"
-
-eval_echo "docker run -d --name $CONTAINER_NAME -e SERVER_IP=$SERVER_IP -e GAME_AI=$GAME_AI -e ADMIN_PASSWORD=$ADMIN_PASSWORD -e CONTEXT=$CONTEXT -e SPRING_PROFILES_ACTIVE=$PROFILES -v $HOME_DIR/database:/usr/app/database -p $SERVER_PORT:8080 $DOCKER_IMAGE"
+mkdir $HOME_DIR/logs
+chown $JETTY_PID:$JETTY_PID $HOME_DIR/logs
+touch $HOME_DIR/logs/codenjoy-contest.log
+chown $JETTY_PID:$JETTY_PID $HOME_DIR/logs/codenjoy-contest.log
+mkdir $HOME_DIR/database
+chown $JETTY_PID:$JETTY_PID $HOME_DIR/database
+docker rm --force $CONTAINER_NAME
+docker run -d --name $CONTAINER_NAME -e SERVER_IP=$SERVER_IP -e GAME_AI=$GAME_AI -e ADMIN_PASSWORD=$ADMIN_PASSWORD -e CONTEXT=$CONTEXT -e SPRING_PROFILES_ACTIVE=$PROFILES -v $HOME_DIR/database:/usr/app/database -p $SERVER_PORT:8080 $DOCKER_IMAGE
 
